2825e9455b37fe4f2a5363b7a999e144170d128a
epineiro <epineiro@bumeran.com>
2015-12-23 12:11:53 -0300

    fix dependencia erronea

--- HandlebarsPluginGrailsPlugin.groovy
+++ HandlebarsPluginGrailsPlugin.groovy
@@ -1,10 +1,9 @@
-import com.navent.handlebars.utils.DefaultJawrLocaleResolver
 import com.navent.handlebars.utils.I18nSpringMessageSource
 
 
 class HandlebarsPluginGrailsPlugin {
     // the plugin version
-    def version = "1.0"
+    def version = "1.0.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
